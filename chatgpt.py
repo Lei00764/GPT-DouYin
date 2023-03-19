03d2e8e9923041f699d94520eda73f636b629b42
lei <xiang.lei.se@foxmail.com>
2023-03-19 20:19:12 +0800

    微调，从api.json中调用api

--- chatgpt.py
+++ chatgpt.py
@@ -1,9 +1,16 @@
 import openai
+import json
 
 # prompt: 要说的话
 # return: GPT-3.5 (即CHAT-GPT) 的答复
 def generate_text(prompt):
-    openai.api_key = "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"
+    # 从 JSON 文件中读取数据
+    with open('api.json') as f:
+        data = json.load(f)
+    # 获取 API URL
+    api = data['api']
+    openai.api_key = api
+
     resp = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
         messages=[
@@ -14,6 +21,5 @@ def generate_text(prompt):
     content = resp['choices'][0]['message']['content']
     content = content.replace('\n', '')
     return content.strip()
-
 
 print(generate_text("请列举5点软件工程专业的优点"))
